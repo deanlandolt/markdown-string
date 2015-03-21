ba1290b6fe45a89416e54e7edc320e8d900700f8
Dean Landolt <dean@deanlandolt.com>
2015-03-20 22:44:52 -0400

    Add deindent

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # markdown-string
+
 Markdown compilation in es6 template strings
 
 ```js
 var md = require('markdown-string')
 
 var args = { exclamation: '!!1' }
+
 md`compiles **markdown** strings to *html* strings${args.exclamation}`
 // 'compiles <strong>markdown</strong> strings to <em>html</em> strings!!1'
 
@@ -12,7 +14,7 @@ md`
 # Multiline Strings
     
 * such
-* wow
+* wow${args.exclamation}
 `
 // '<h1>Multiline Strings</h1>\n<ul>\n<li>such</li>\n<li>wow</li>\n</ul>\n'
 ```
--- index.js
+++ index.js
@@ -2,12 +2,14 @@
 // dervied from https://github.com/brianleroux/micromark
 
 var commonmark = require('commonmark');
+var deindent = require('deindent');
+
 var reader = new commonmark.DocParser();
 var writer = new commonmark.HtmlRenderer();
 var LINE_BREAK = /\n/;
 var P_TAG_WRAP = /^<p>(.*)<\/p>\r?\n?$/i;
 
-module.exports = function markdownTag(strings) {
+function markdownTag(strings) {
   var input = strings[0];
   for (var i = 1, length = arguments.length; i < length; ++i) {
     input += arguments[i] + strings[i];
@@ -24,3 +26,5 @@ module.exports = function markdownTag(strings) {
 
   return parsed;
 };
+
+module.exports = deindent(markdownTag);
